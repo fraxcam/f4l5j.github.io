c5a95ed09a0a374a95be6f0a1c7bfb8cb85eaeb4
f4l5j <78499844+F4L5J@users.noreply.github.com>
2021-05-16 18:17:17 +0200

    Add files via upload

--- main2.js
+++ main2.js
@@ -1,7 +1,7 @@
 var objPeople = [
 	{ // Object @ 0 index
-		username: "test",
-		password: "pizza"
+		username: "orgoglio",
+		password: "bresciano"
 	},/*
 	{ // Object @ 1 index
 		username: "matt",
@@ -13,19 +13,24 @@ var objPeople = [
 	}
 */
 ]
-
+function myFunction() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
 function getInfo() {
 	var username = document.getElementById('username').value
 	var password = document.getElementById('password').value
 
 	for(var i = 0; i < objPeople.length; i++) {
-		// check is user input matches username and password of a current index of the objPeople array
 		if(username == objPeople[i].username && password == objPeople[i].password) {
-			console.log(username + " is logged in!!!")
+			alert("Salve e Benvenuto nella sezione test");
       window.open ("lollo/index.html");
-			// stop the function if this is found to be true
 			return
 		}
 	}
-	console.log("incorrect username or password")
+	alert("ACCESSO NEGATO");
 }
