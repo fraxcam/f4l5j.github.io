30380b51e6ae31fb52843ffb363ac2855ebcc419
f4l5j <78499844+F4L5J@users.noreply.github.com>
2021-06-09 09:34:32 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-var cookieContainer=document.querySelector(".cookie-container"),cookieButton=document.querySelector(".cookie-btn");cookieButton.addEventListener("click",function(){cookieContainer.classList.remove("active");localStorage.setItem("cookieBannerDisplayed","true")});setTimeout(function(){localStorage.getItem("cookieBannerDisplayed")||cookieContainer.classList.add("active")},2E3);
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
+window.ondragstart = function() {return false}
